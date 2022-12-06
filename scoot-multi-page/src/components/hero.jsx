ae08f48cb6abd3b5fac6451668f484140dea844a
EJ Milord <jmilord7@gmail.com>
2022-12-06 13:41:11 -0500

    initial commit

--- scoot-multi-page/src/components/hero.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function hero() {
-  return (
-    <h1>Hero Section</h1>
-  )                         
-}
--- scoot-multi-page/src/components/navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import logo from "../assets/logo.svg";
-export default function navbar() {
-  return (
-    <h1>NavBar Here</h1>
-  );
-}
